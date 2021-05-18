7f834242c1755321514524fcced83722856dc312
Billy Portela <billy.portela00@gmail.com>
2021-05-18 12:36:55 +0000

    New models, tests and documentation on 18/05/2021

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,7 +1,3 @@
-{{ config (
-    materialized="table"
-)}}
-
 with customers as (
 
     select * from {{ ref('stg_customers')}}
@@ -9,23 +5,19 @@ with customers as (
 ),
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 
 ),
-
 customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
 ),
 
 
@@ -37,10 +29,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
 
 )
